3f46b6eda4fa942a008ccf4ce650a72455b1d876
Marcello Bastéa-Forte <marcello@descript.com>
2020-11-11 09:52:07 -0800

    run both local and remote

--- renderer.js
+++ renderer.js
@@ -2,10 +2,14 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const childProcess = require('electron').remote.require('child_process');
+const remoteChildProcess = require('electron').remote.require('child_process');
+const childProcess = require('child_process');
 const {spawnTest} = require('./spawn-test')
 
 const element = document.getElementById('output');
+spawnTest(remoteChildProcess, (message) => {
+    element.textContent += `REMOTE child_process: ${message}\n`;
+});
 spawnTest(childProcess, (message) => {
-    element.textContent += `${message}\n`;
+    element.textContent += `LOCAL child_process: ${message}\n`;
 });
